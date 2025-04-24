5376dab21d0a2174a7718adf0bd25fa10d97990a
FelipeDgrandel <felipe.veigarpm@gmail.com>
2025-04-23 22:16:30 -0300

    feat: adiciona tela principal com Header e Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
+      <main style={{ padding: '20px' }}>
+        <p>Bem-vindo à aplicação! Essa é a tela principal.</p>
+      </main>
+      <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
